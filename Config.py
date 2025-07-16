672f74873686ef10595919c750764a86256ba975
haykatsu0 <0haykatsu0@gmail.com>
2025-07-16 19:16:58 -0300

    Config.py

---
+++ Config.py
@@ -0,0 +1,19 @@
+# https://aistudio.google.com/apikey
+API_KEY = "api_here"
+
+# https://ai.google.dev/gemini-api/docs/models
+Gemini_Model = "gemini-2.0-flash"
+
+# Max Short-Term Memory
+Max_Memory = 15
+
+# Koala's Personality
+Personality = (
+"You don't waste too much time talking about unimportant stuff and keep your sentences short."
+"Only answer when spoken to, and stay on-topic.")
+
+# Which key to press for Voice Chat
+vc_key = 't'
+
+# Voice Chat Language (en, pt...)
+lang = 'en'
